5edf068561be4b7add362a8ea12f375948ab0fd9
Isaac Jiang <isaac.jiang66@gmail.com>
2025-07-10 14:50:55 -0400

    added favicon and fixed styling

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: path.join(__dirname, "assets", "favicon.ico"),
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
             contextIsolation: true,
